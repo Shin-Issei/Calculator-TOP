d38e9f43d2150a0d60bb8ca7f2333a113e327e23
Jordan <JMCollins0@aol.com>
2022-04-20 11:37:16 -0400

    Starting the script solution over.

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const test = document.querySelector(".num-btn");
-test.addEventListener("click", () => {console.log(test.textContent)}); // This is a string representation
 
-// TODO: Be able to add any amount of numbers of any size we like and console.log them
