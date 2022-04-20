4098c3f220f485fa450c209ec25a32dff55ad183
Jordan <JMCollins0@aol.com>
2022-04-20 12:01:38 -0400

    Add display functionality to buttons

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const display = document.querySelector("#display");
+const numberButtons = document.querySelectorAll(".num-btn");
+numberButtons.forEach(btn => {
+    btn.addEventListener("click", () =>{
+        // display.textContent = Number(btn.textContent);
+       display.textContent = Number(btn.textContent)
+    })
+})
