ac9ba845341cae01cee493475ac740f1d0acc50c
Jordan <JMCollins0@aol.com>
2022-04-20 15:19:10 -0400

    Seperate concerns across eventlisteners
    Each category of button has its own event listeners.

--- script.js
+++ script.js
@@ -1,8 +1,79 @@
 const display = document.querySelector("#display");
-const numberButtons = document.querySelectorAll(".num-btn");
-numberButtons.forEach(btn => {
+const NumberButtons = document.querySelectorAll(".btn");
+const SymbolButtons = document.querySelectorAll(".sym-btn");
+const Equals = document.querySelector("#equals");
+const operatorSymbols = ["+", "-", "*", "/"];
+let operandArr = [];
+let opOneNum ;
+let opTwoNum ;
+let operation ;
+NumberButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (!operatorSymbols.includes(btn.textContent)) { // If its a number then excecute
+      operandArr.push(btn.textContent);
+      let opOneSTR = operandArr.join("");
+      display.textContent = opOneSTR;
+    }
+   
+});
+});
+
+SymbolButtons.forEach((btn) =>{
     btn.addEventListener("click", () =>{
-        // display.textContent = Number(btn.textContent);
-       display.textContent = Number(btn.textContent)
-    })
+            operation = btn.textContent;
+            if(typeof(opOneNum) === "undefined"){
+            opOneNum = Number(display.textContent);
+            console.log("the first operand is " + opOneNum);
+            }
+            operandArr = [];
+            display.textContent = ""; 
+            console.log(operation);
+        })
+    });
+
+
+Equals.addEventListener("click", () => {
+    if(typeof(opOneNum) !== "undefined"){
+        opTwoNum = Number(display.textContent);
+        console.log("the secdond operand is " + opTwoNum);
+    }
+
+    if (operation === "+"){
+        let ans = opOneNum + opTwoNum
+        ClearAll();
+        return display.textContent = ans;
+    }
+    if (operation === "-"){
+        let ans = opOneNum - opTwoNum
+        ClearAll();
+        return display.textContent = ans;
+    }
+    if (operation === "*"){
+        let ans = opOneNum * opTwoNum
+        ClearAll();
+        return display.textContent = ans;
+    }
+    if (operation === "/"){
+        let ans = opOneNum / opTwoNum
+        ClearAll();
+        return display.textContent = ans;
+    }
 })
+    
+function ClearAll (){
+    operandArr = [];
+    opOneSTR = "";
+    opOneNum = undefined;
+    opTwoNum = undefined;
+}
+
+
+//   if (btn.textContent === "+") {
+//    opTwoNum = Number(display.textContent)
+//       add(opOneNum, opTwoNum)
+//     console.log("You chose addition");
+
+//   }
+//   if (btn.textContent === "-") {
+//     console.log("You chose subtraction");
+//   }
