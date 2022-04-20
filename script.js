e2dc8d598577aa4355e1d75ceedc1f36bf93fefb
Jordan <JMCollins0@aol.com>
2022-04-20 09:56:36 -0400

    Add html js and css files along with boilerplate
    HTML: Add buttons to be tested
    JS: Add test eventlisteners for math function
    CSS: ...

---
+++ script.js
@@ -0,0 +1,4 @@
+const test = document.querySelector(".num-btn");
+test.addEventListener("click", () => {console.log(test.textContent)}); // This is a string representation
+
+// TODO: Be able to add any amount of numbers of any size we like and console.log them
