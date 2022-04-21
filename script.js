dd045f92a419aabad8f1d854afe65c8dcc546790
Jordan <JMCollins0@aol.com>
2022-04-21 08:57:34 -0400

    Code Formatting

--- script.js
+++ script.js
@@ -4,69 +4,70 @@ const SymbolButtons = document.querySelectorAll(".sym-btn");
 const Equals = document.querySelector("#equals");
 const operatorSymbols = ["+", "-", "*", "/"];
 let operandArr = [];
-let opOneNum ;
-let opTwoNum ;
-let operation ;
+let opOneNum;
+let opTwoNum;
+let operation;
+// The number buttons don't store anything yet. It only displays to the screen
 NumberButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (!operatorSymbols.includes(btn.textContent)) { // If its a number then excecute
+    if (!operatorSymbols.includes(btn.textContent)) {
+      // If its a number then excecute
       operandArr.push(btn.textContent);
       let opOneSTR = operandArr.join("");
       display.textContent = opOneSTR;
     }
-   
-});
+  });
 });
 
-SymbolButtons.forEach((btn) =>{
-    btn.addEventListener("click", () =>{
-            operation = btn.textContent;
-            if(typeof(opOneNum) === "undefined"){
-            opOneNum = Number(display.textContent);
-            console.log("the first operand is " + opOneNum);
-            }
-            operandArr = [];
-            display.textContent = ""; 
-            console.log(operation);
-        })
-    });
+// The number only needs to be stored as an operand once an operation is chosen.
 
+SymbolButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    operation = btn.textContent;
+    if (typeof opOneNum === "undefined") {
+      opOneNum = Number(display.textContent);
+      console.log("the first operand is " + opOneNum);
+    }
+    operandArr = [];
+    display.textContent = "";
+    console.log(operation);
+  });
+});
 
 Equals.addEventListener("click", () => {
-    if(typeof(opOneNum) !== "undefined"){
-        opTwoNum = Number(display.textContent);
-        console.log("the secdond operand is " + opTwoNum);
-    }
+  if (typeof opOneNum !== "undefined") {
+    opTwoNum = Number(display.textContent);
+    console.log("the secdond operand is " + opTwoNum);
+  }
 
-    if (operation === "+"){
-        let ans = opOneNum + opTwoNum
-        ClearAll();
-        return display.textContent = ans;
-    }
-    if (operation === "-"){
-        let ans = opOneNum - opTwoNum
-        ClearAll();
-        return display.textContent = ans;
-    }
-    if (operation === "*"){
-        let ans = opOneNum * opTwoNum
-        ClearAll();
-        return display.textContent = ans;
-    }
-    if (operation === "/"){
-        let ans = opOneNum / opTwoNum
-        ClearAll();
-        return display.textContent = ans;
-    }
-})
-    
-function ClearAll (){
-    operandArr = [];
-    opOneSTR = "";
-    opOneNum = undefined;
-    opTwoNum = undefined;
+  if (operation === "+") {
+    let ans = opOneNum + opTwoNum;
+    ClearAll();
+    return (display.textContent = ans);
+  }
+  if (operation === "-") {
+    let ans = opOneNum - opTwoNum;
+    ClearAll();
+    return (display.textContent = ans);
+  }
+  if (operation === "*") {
+    let ans = opOneNum * opTwoNum;
+    ClearAll();
+    return (display.textContent = ans);
+  }
+  if (operation === "/") {
+    let ans = opOneNum / opTwoNum;
+    ClearAll();
+    return (display.textContent = ans);
+  }
+});
+
+function ClearAll() {
+  operandArr = [];
+  opOneSTR = "";
+  opOneNum = undefined;
+  opTwoNum = undefined;
 }
-
 
 //   if (btn.textContent === "+") {
 //    opTwoNum = Number(display.textContent)
