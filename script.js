314104400d523c8c63a3664d8fda54eb264ebb9c
Jordan <JMCollins0@aol.com>
2022-04-21 15:49:56 -0400

    Total Solution Reset

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ let operandArr = [];
 let opOneNum;
 let opTwoNum;
 let operation;
-
+const whiteSpace = display.textContent; // The nbsp will stop the numbers from bouncing up and down.
+let localOperation;
+let flag = false;
 //All Event Handlers
 
 // The number buttons don't need to store anything since we just need to push the series of numbers to the screen.
@@ -24,60 +26,77 @@ NumberButtons.forEach((btn) => {
   });
 });
 
-
 // The number only needs to be stored as an operand once an operation is chosen.
 SymbolButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    operation = btn.textContent; // The 'equals' function needs to know this
-    if (typeof opOneNum === "undefined") {
-      opOneNum = Number(display.textContent);
-      console.log("the first operand is " + opOneNum);
-    }
-    operandArr = [];
-    display.textContent = "";
-    // console.log(operation); 
-  });
+  btn.addEventListener("click", () => {});
 });
 
 Equals.addEventListener("click", () => {
+  // FnEquals(operation);
+});
+
+ClearScrn.addEventListener("click", () => {
+  ClearAll();
+});
+
+function ClearAll() {
+  display.textContent = whiteSpace;
+  operandArr = [];
+  opOneSTR = "";
+  opOneNum = undefined;
+  opTwoNum = undefined;
+  console.log(whiteSpace);
+}
+
+function symbolPress() {
+  if (typeof opOneNum === "undefined") {
+    opOneNum = Number(display.textContent);
+    console.log("the first operand is " + opOneNum);
+  }
+  if (typeof localOperation === "undefined") {
+    localOperation = btn.textContent;
+  } else {
+    console.log(
+      `The local operantion is defined already, it's ${localOperation}`
+    );
+    opTwoNum = display.textContent;
+    FnEquals(localOperation);
+    localOperation = btn.textContent;
+  }
+  operation = btn.textContent; // The 'equals' function needs to know this
+  // let interOperation = btn.textContent
+  operandArr = [];
+  // display.textContent = whiteSpace;
+  // console.log(operation);
+}
+
+function FnEquals() {
   if (typeof opOneNum !== "undefined") {
     opTwoNum = Number(display.textContent); // *Now* we can read the second
     console.log("the secdond operand is " + opTwoNum);
   }
 
-  if (operation === "+") {
+  if (arguments[0] === "+") {
     let ans = opOneNum + opTwoNum;
     ClearAll();
     return (display.textContent = ans);
   }
-  if (operation === "-") {
+  if (arguments[0] === "-") {
     let ans = opOneNum - opTwoNum;
     ClearAll();
     return (display.textContent = ans);
   }
-  if (operation === "*") {
+  if (arguments[0] === "*") {
     let ans = opOneNum * opTwoNum;
     ClearAll();
     return (display.textContent = ans);
   }
-  if (operation === "/") {
-    let ans = Math.round((opOneNum / opTwoNum) * 100)/100 ;
-    if(opTwoNum === 0){
-      return display.textContent = "No."
+  if (arguments[0] === "/") {
+    let ans = Math.round((opOneNum / opTwoNum) * 100) / 100;
+    if (opTwoNum === 0) {
+      return (display.textContent = "No.");
     }
     ClearAll();
     return (display.textContent = ans);
   }
-});
-
-ClearScrn.addEventListener("click", () => {ClearAll()});
-
-function ClearAll() {
-  display.textContent = "";
-  operandArr = [];
-  opOneSTR = "";
-  opOneNum = undefined;
-  opTwoNum = undefined;
-  console.log("clear")
-  
 }
