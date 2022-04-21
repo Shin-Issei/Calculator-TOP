9a335ea61d70f1a73dadf9f1a86d4aa6a89dcfe5
Jordan <JMCollins0@aol.com>
2022-04-21 13:03:08 -0400

    Flexbox-grid style hybrid is now flexbox only

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// Nessecary DOM references
 const display = document.querySelector("#display");
 const NumberButtons = document.querySelectorAll(".btn");
 const SymbolButtons = document.querySelectorAll(".sym-btn");
 const Equals = document.querySelector("#equals");
+const ClearScrn = document.querySelector("#clear");
+// Global Variables
 const operatorSymbols = ["+", "-", "*", "/"];
 let operandArr = [];
 let opOneNum;
 let opTwoNum;
 let operation;
-// The number buttons don't store anything yet. It only displays to the screen
+
+//All Event Handlers
+
+// The number buttons don't need to store anything since we just need to push the series of numbers to the screen.
 NumberButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     if (!operatorSymbols.includes(btn.textContent)) {
-      // If its a number then excecute
       operandArr.push(btn.textContent);
       let opOneSTR = operandArr.join("");
       display.textContent = opOneSTR;
@@ -19,24 +24,24 @@ NumberButtons.forEach((btn) => {
   });
 });
 
-// The number only needs to be stored as an operand once an operation is chosen.
 
+// The number only needs to be stored as an operand once an operation is chosen.
 SymbolButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    operation = btn.textContent;
+    operation = btn.textContent; // The 'equals' function needs to know this
     if (typeof opOneNum === "undefined") {
       opOneNum = Number(display.textContent);
       console.log("the first operand is " + opOneNum);
     }
     operandArr = [];
     display.textContent = "";
-    console.log(operation);
+    // console.log(operation); 
   });
 });
 
 Equals.addEventListener("click", () => {
   if (typeof opOneNum !== "undefined") {
-    opTwoNum = Number(display.textContent);
+    opTwoNum = Number(display.textContent); // *Now* we can read the second
     console.log("the secdond operand is " + opTwoNum);
   }
 
@@ -56,25 +61,23 @@ Equals.addEventListener("click", () => {
     return (display.textContent = ans);
   }
   if (operation === "/") {
-    let ans = opOneNum / opTwoNum;
+    let ans = Math.round((opOneNum / opTwoNum) * 100)/100 ;
+    if(opTwoNum === 0){
+      return display.textContent = "No."
+    }
     ClearAll();
     return (display.textContent = ans);
   }
 });
 
+ClearScrn.addEventListener("click", () => {ClearAll()});
+
 function ClearAll() {
+  display.textContent = "";
   operandArr = [];
   opOneSTR = "";
   opOneNum = undefined;
   opTwoNum = undefined;
+  console.log("clear")
+  
 }
-
-//   if (btn.textContent === "+") {
-//    opTwoNum = Number(display.textContent)
-//       add(opOneNum, opTwoNum)
-//     console.log("You chose addition");
-
-//   }
-//   if (btn.textContent === "-") {
-//     console.log("You chose subtraction");
-//   }
